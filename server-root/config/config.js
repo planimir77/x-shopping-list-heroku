8f43797d6391f387a97603d43387586edd32300a
planimir77 <planimir7708@gmail.com>
2021-03-30 12:45:11 +0300

    add real database url in development

--- server-root/config/config.js
+++ server-root/config/config.js
@@ -3,13 +3,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 4200,
-        dbURL: 'mongodb://localhost:27017/shoppinglists',
-        origin: [
-            'http://localhost:5555', 
-            'http://localhost:4200', 
-            'http://192.168.0.102:4200',
-            'http://192.168.1.145:4200',
-        ]
+        dbURL: process.env.DB_URL_CREDENTIALS,
+        origin: []
     },
     production: {
         port: process.env.PORT || 4200,
