09d61659a48559bca1a7decdb35674c648c73fea
planimir77 <planimir7708@gmail.com>
2021-03-31 17:29:32 +0300

    remove: unnecessary routes

--- server-root/controllers/index.js
+++ server-root/controllers/index.js
@@ -1,13 +1,9 @@
 const authController = require('./auth');
-const themeController = require('./themeController');
-const postController = require('./postController');
 const shoppinglistController = require('./shoppinglistController');
 const itemController = require('./itemController');
 
 module.exports = {
     authController,
-    themeController,
-    postController,
     shoppinglistController,
     itemController
 }
--- server-root/router/index.js
+++ server-root/router/index.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const users = require('./users');
-const themes = require('./themes');
-const posts = require('./posts');
-const likes = require('./likes');
 const test = require('./test');
 const shoppinglists = require('./shoppinglists');
 const item = require('./item');
@@ -10,11 +7,6 @@ const item = require('./item');
 router.use('/shoppinglists', shoppinglists);
 router.use('/item', item);
 router.use('/users', users);
-router.use('/themes', themes);
-router.use('/posts', posts);
-router.use('/likes', likes);
 router.use('/test', test);
-
-router.use('/*', test);
 
 module.exports = router;
