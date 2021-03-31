82e309e25f5293c4187a5df281ab52a3af0fd08a
planimir77 <planimir7708@gmail.com>
2021-03-31 12:49:46 +0300

    config origin

--- server-root/config/config.js
+++ server-root/config/config.js
@@ -4,7 +4,7 @@ const config = {
     development: {
         port: process.env.PORT || 4200,
         dbURL: process.env.DB_URL_CREDENTIALS,
-        origin: ['https://x-shopping-list.herokuapp.com']
+        origin: ['http://localhost:5000']
     },
     production: {
         port: process.env.PORT || 4200,
--- server-root/config/express.js
+++ server-root/config/express.js
@@ -38,7 +38,7 @@ module.exports = (app) => {
     }));
 
     app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', config.origin);
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, access-control-allow-origin');
