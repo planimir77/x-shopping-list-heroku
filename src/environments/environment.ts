95157c137d63f8f922d6a9159b94ba99855768a1
planimir77 <planimir7708@gmail.com>
2024-05-14 21:45:40 +0300

    update: api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiUrl: `${window.location.origin}/api`,
+  apiUrl: 'https://x-shopping-list-api.vercel.app/api'
+  //apiUrl: `${window.location.origin}/api`,
 };
 
 /*
